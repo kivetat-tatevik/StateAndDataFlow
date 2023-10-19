9770923e7a1ce1a4feb285c2cd224d8fe5043385
kivetat-tatevik <kivetat.85@yandex.ru>
2023-10-19 19:32:32 +0200

    Initial Commit

---
+++ StateAndDataFlow/StateAndDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowApp.swift
+//  StateAndDataFlow
+//
+//  Created by Tatevik Khunoyan on 19.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
